609d5ac3ce5f81ed2597a5eb8c6bdcd3d6bf4a14
Jacob Pfeifer <jakepf00@gmail.com>
2024-01-17 21:25:45 -0600

    Use hook to load rep list

--- src/components/Repertoire.tsx
+++ src/components/Repertoire.tsx
@@ -1,23 +1,28 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import RepListCSV from '../Assets/Files/RepertoireList.csv'
 
 const Repertoire: FC<{}> = () => {
-    const [ text, setText ] = useState<any | null>(null);
+    const [text, setText] = useState<any | null>(null);
 
-    const load = function(){
+    const loadCSV = function () {
         fetch(RepListCSV)
-            .then( response => response.text() )
-            .then( responseText => {
-                setText( responseText );
+            .then(response => response.text())
+            .then(responseText => {
+                setText(responseText);
             })
     };
+
+    useEffect(() => {
+        let ignore = false;
+        if (!ignore) loadCSV()
+        return () => { ignore = true; }
+    }, []);
 
     return (
         <section id="repertoire">
             <h1>Rep list</h1>
-            <button onClick={ load }>load</button>
             <h2>Rep:</h2>
-            <pre>{ text }</pre>
+            <pre>{text}</pre>
         </section>
     );
 };
