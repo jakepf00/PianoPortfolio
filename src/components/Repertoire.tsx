37b9bb4eb81f913cc8b36ea91e337af0823fb8b5
Jacob Pfeifer <jakepf00@gmail.com>
2024-01-17 18:39:00 -0600

    Load rep list from csv file

--- src/components/Repertoire.tsx
+++ src/components/Repertoire.tsx
@@ -1,9 +1,23 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import RepListCSV from '../Assets/Files/RepertoireList.csv'
 
 const Repertoire: FC<{}> = () => {
+    const [ text, setText ] = useState<any | null>(null);
+
+    const load = function(){
+        fetch(RepListCSV)
+            .then( response => response.text() )
+            .then( responseText => {
+                setText( responseText );
+            })
+    };
+
     return (
         <section id="repertoire">
             <h1>Rep list</h1>
+            <button onClick={ load }>load</button>
+            <h2>Rep:</h2>
+            <pre>{ text }</pre>
         </section>
     );
 };
