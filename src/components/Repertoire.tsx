0e6be2beeacb66e38c0ee01441a23883bef7baff
Jacob Pfeifer <jakepf00@gmail.com>
2024-01-22 11:54:29 -0600

    Rep list table formatting

--- src/components/Repertoire.tsx
+++ src/components/Repertoire.tsx
@@ -1,6 +1,7 @@
 import Papa, { ParseResult } from "papaparse"
 import React, { FC, useEffect, useState } from "react";
 import RepListCSV from '../Assets/Files/RepertoireList.csv'
+import Table from 'react-bootstrap/Table';
 
 
 type piece = {
@@ -40,23 +41,25 @@ const Repertoire: FC<{}> = () => {
 
     return (
         <section id="repertoire">
-            <h1>Rep list</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Composer</th>
-                        <th>Piece</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {repertoire?.pieces.map((piece, index) => (
-                        <tr key={index}>
-                            <td>{piece.composer}</td>
-                            <td>{piece.name}</td>
+            <div className="container">
+                <h1>Rep list</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Composer</th>
+                            <th>Piece</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {repertoire?.pieces.map((piece, index) => (
+                            <tr key={index}>
+                                <td>{piece.composer}</td>
+                                <td>{piece.name}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>
         </section>
     );
 };
