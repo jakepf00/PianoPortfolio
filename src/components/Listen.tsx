f8a60e600c2ffb0f461ffcae998b80678d5958ef
Jacob Pfeifer <jakepf00@gmail.com>
2024-01-29 14:25:00 -0600

    Add videos to listen page

--- src/components/Listen.tsx
+++ src/components/Listen.tsx
@@ -1,24 +1,52 @@
+import Container from "react-bootstrap/Container"
+import Col from "react-bootstrap/Col"
+import Image from "react-bootstrap/Image"
 import React, { FC } from "react";
+import Row from "react-bootstrap/Row"
+import '../CSS/Video.css';
 
 const Listen: FC<{}> = () => {
     return (
         <section id="listen">
             <h1>Featured Videos</h1>
-            <div className="container">
-                <div className="row">
-                    <div className="embed-responsive embed-responsive-16by9">
-                        <iframe 
-                        className="embed-responsive-item"
-                        src="https://youtube.com/embed/VGSB2iKT9Ws?si=KvkS1TpXaaQDrV1w"
-                        allowFullScreen />
-                    </div>
-                    <div className="embed-responsive embed-responsive-16by9">
-                        <iframe
-                        src="https://youtube.com/embed/VGSB2iKT9Ws?si=KvkS1TpXaaQDrV1w"
-                        allowFullScreen />
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="video-container">
+                            <Image src="https://placehold.co/160x90" width="100%" style={{ visibility: "hidden" }} />
+                            <iframe
+                                src="https://youtube.com/embed/VGSB2iKT9Ws"
+                                allowFullScreen />
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="video-container">
+                            <Image src="https://placehold.co/160x90" width="100%" style={{ visibility: "hidden" }} />
+                            <iframe
+                                src="https://youtube.com/embed/6dQvl02_IvQ"
+                                allowFullScreen />
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="video-container">
+                            <Image src="https://placehold.co/160x90" width="100%" style={{ visibility: "hidden" }} />
+                            <iframe
+                                src="https://youtube.com/embed/DDUeJZBZdJY"
+                                allowFullScreen />
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="video-container">
+                            <Image src="https://placehold.co/160x90" width="100%" style={{ visibility: "hidden" }} />
+                            <iframe
+                                src="https://youtube.com/embed/ZWKlFfFj6xs"
+                                allowFullScreen />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </section>
     );
 };
